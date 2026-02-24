9eec480f1e99f186ffe5065592d16fec27d1f2b0
azaliya <azaliabrgmv@gmail.com>
2026-02-24 18:44:25 +0300

    Added texbox, label, button

--- project/project/MainWindow.xaml.cs
+++ project/project/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace project
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
